839190724907617e35d84e6a9034c2c0e0f549ba
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2025-04-09 15:01:44 +0900

    Time: 326 ms (96.52%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,12 +1,9 @@
 # Write your MySQL query statement below
-with cte as (
-    select *,count(*) as cnt
-    from Employee
-    where managerId IS NOT NULL
+select name 
+from Employee 
+where id in (
+    select managerId 
+    from employee
     group by managerId
+    having count(managerId)>=5
 )
-
-select e.name
-from Employee e, cte
-where cte.cnt >= 5
-and cte.managerId = e.id
