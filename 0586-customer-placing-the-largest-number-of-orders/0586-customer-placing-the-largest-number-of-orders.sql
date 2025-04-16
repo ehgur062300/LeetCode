3617cbf57081c15958128ef08c7dd446eade7055
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2025-04-16 20:49:03 +0900

    Time: 476 ms (85.63%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,6 +1,18 @@
-# Write your MySQL query statement below
-select customer_number
-from Orders
-group by customer_number
-order by count(*) desc
-limit 1
+-- # Write your MySQL query statement below
+with cte as (
+    select 
+        MAX(o1.cnt) as max_cnt
+    from (
+        select customer_number, count(*) as cnt
+        from Orders
+        group by customer_number
+    ) as o1
+)
+
+select o2.customer_number 
+from (
+    select customer_number, count(*) as cnt
+    from Orders
+    group by customer_number
+) o2, cte c
+where o2.cnt = c.max_cnt
