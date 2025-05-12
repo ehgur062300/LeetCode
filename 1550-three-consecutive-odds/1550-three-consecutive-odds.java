175451fddbfb3f117f8b6c91237d962f449842eb
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2025-05-12 14:39:51 +0900

    Time: 0 ms (100%), Space: 42.2 MB (85.46%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.java
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        for (int i = 2; i<arr.length; i++) {
+        int len = arr.length;
+        for (int i = 2; i<len; i++) {
             if (arr[i-2] % 2 == 1 && arr[i-1] % 2 == 1 && arr[i] % 2 == 1) {
                 return true;
             }
