8d08b9b1788b625f46fce2b3d69707f162ff464d
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2025-05-31 12:41:28 +0900

    Time: 18 ms (67.34%), Space: 44.8 MB (86.66%) - LeetHub

--- 1117-building-h2o/1117-building-h2o.java
+++ 1117-building-h2o/1117-building-h2o.java
@@ -1,25 +1,18 @@
 class H2O {
-    private AtomicInteger count = new AtomicInteger(0);
+    private Semaphore hydrogenSemaphore = new Semaphore(2); // 수소 2개까지 허용
+    private Semaphore oxygenSemaphore = new Semaphore(0);   // 산소는 대기 상태
 
-    public H2O() {
-        
+    public H2O() {}
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        hydrogenSemaphore.acquire();        // 수소 2개까지만 실행됨
+        releaseHydrogen.run();              // 수소 출력
+        oxygenSemaphore.release();          // 산소를 위한 신호 보냄
     }
 
-    public synchronized void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
-        while (count.get() == 2) {
-            wait();
-        }
-        count.getAndIncrement();
-        releaseHydrogen.run();
-        notifyAll();
-    }
-
-    public synchronized void oxygen(Runnable releaseOxygen) throws InterruptedException {
-        while (count.get() != 2) {
-            wait();
-        }
-        count.set(0);
-        releaseOxygen.run();    
-        notifyAll();
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        oxygenSemaphore.acquire(2);         // 수소 2개 신호를 기다림
+        releaseOxygen.run();                // 산소 출력
+        hydrogenSemaphore.release(2);       // 다음 수소 2개를 위해 자원 돌려줌
     }
 }
